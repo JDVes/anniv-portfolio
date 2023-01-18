25b58633a86c1f214c8021bf6c822f5865a7a07f
Jean-David DOUTI <88549535+JDVes@users.noreply.github.com>
2023-01-18 21:29:53 +0000

    Done front-end

--- js/script.js
+++ js/script.js
@@ -1,4 +1,18 @@
 const header = document.querySelector("header");
 window.addEventListerner ("scroll" , function() {
     header.classList.toggle ("sticky" , window.scrollY > 100)
-})
+});
+
+
+let menu = document.querySelector('#menu-icon');
+let navlist = document.querySelector('.navlist');
+menu.onclick = () => {
+    menu.classList.toggle('bx-x');
+    navlist.classList.toggle('open');
+
+};
+
+window.onscroll = () => {
+    menu.classList.remove('bx-x')
+    menu.classList.remove('open')
+}
